893902b1cd12861bf34e6e3da75dd92604a7d46f
vsemusvetu <vsemusvetu@mail.ru>
2022-01-24 16:42:45 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,37 @@
+﻿Class MainWindow
+    Private Sub btn_Click(sender As Object, e As RoutedEventArgs) Handles btn.Click
+        MessageBox.Show("Button 1 is Clicked")
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As RoutedEventArgs) Handles btn1.Click
+        MessageBox.Show("Button 2 is Clicked")
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As RoutedEventArgs) Handles btn2.Click
+        MessageBox.Show("Button 3 is Clicked")
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As RoutedEventArgs) Handles btn3.Click
+        MessageBox.Show("Button 4 is Clicked")
+    End Sub
+
+    Private Sub ListBox_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
+
+    End Sub
+
+    Private Sub RichTextBox_TextChanged(sender As Object, e As TextChangedEventArgs)
+
+    End Sub
+
+    Private Sub ComboBox_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
+
+    End Sub
+
+    Private Sub RichTextBox_TextChanged_1(sender As Object, e As TextChangedEventArgs)
+
+    End Sub
+
+    Private Sub RichTextBox_TextChanged_2(sender As Object, e As TextChangedEventArgs)
+
+    End Sub
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,59 @@
+﻿Imports System
+Imports System.Globalization
+Imports System.Reflection
+Imports System.Resources
+Imports System.Runtime.InteropServices
+Imports System.Windows
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("Kaspersky_Internet_Security")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Kaspersky_Internet_Security")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: ComVisible(false)>
+
+'Чтобы начать создание локализуемых приложений, задайте
+'<UICulture>ТребуемоеЗначение</UICulture> в файле .vbproj
+'внутри <PropertyGroup>.  Например, если используется английский США
+'в исходных файлах установите значение <UICulture> равным "en-US".  Затем снимите комментарий
+'с атрибута NeutralResourceLanguage (ниже).  Замените "en-US" в расположенной ниже
+'строке значением, соответствующим параметру UICulture в файле проекта.
+
+'<Assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)>
+
+
+'Атрибут ThemeInfo указывает расположение словарей ресурсов для конкретной темы и словарей общих ресурсов.
+'1-й параметр: расположение словарей ресурсов для конкретной темы
+'(используется, если ресурс не найден на странице,
+' или в словарях ресурсов приложения)
+
+'2-й параметр: расположение словаря общих ресурсов
+'(используется, если ресурс не найден на странице,
+'в приложении и в словарях ресурсов для конкретной темы)
+<Assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)>
+
+
+
+'Следующий идентификатор GUID представляет идентификатор typelib, если этот проект доступен через COM
+<Assembly: Guid("3abb50a8-8469-4c33-8689-f9e5a089962e")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номера сборки и редакции по умолчанию 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
